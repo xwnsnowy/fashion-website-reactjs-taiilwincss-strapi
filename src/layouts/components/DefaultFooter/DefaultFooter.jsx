51053aaf488b367d3b3b43baa2bf921c209489fb
Nguyen Tien Thanh <thanhnthe163528@fpt.edu.vn>
2024-01-20 14:29:34 +0700

    feat: implement DefaultFooter  component

--- src/layouts/components/DefaultFooter/DefaultFooter.jsx
+++ src/layouts/components/DefaultFooter/DefaultFooter.jsx
@@ -1,5 +1,125 @@
+// import footerLogo from "../../assets/logo.png";
+// import Banner from "../../assets/website/footer-pattern.jpg";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+  FaMobileAlt,
+} from "react-icons/fa";
+
+// const BannerImg = {
+//   backgroundImage: `url(${Banner})`,
+//   backgroundPosition: "bottom",
+//   backgroundRepeat: "no-repeat",
+//   backgroundSize: "cover",
+//   height: "100%",
+//   width: "100%",
+// };
+
 const DefaultFooter = () => {
-  return <div></div>;
+  const FooterLinks = [
+    {
+      title: "Home",
+      link: "/#",
+    },
+    {
+      title: "About",
+      link: "/#about",
+    },
+    {
+      title: "Contact",
+      link: "/#contact",
+    },
+    {
+      title: "Blog",
+      link: "/#blog",
+    },
+  ];
+
+  return (
+    <div className="text-black">
+      <div className="container">
+        <div data-aos="zoom-in" className="grid pb-44 pt-5 md:grid-cols-3">
+          {/* company details */}
+          <div className="px-4 py-8">
+            <h1 className="mb-3 flex items-center gap-3 text-justify text-xl font-bold sm:text-left sm:text-3xl">
+              {/* <img src={footerLogo} alt="" className="max-w-[50px]" />
+              Shopsy */}
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum in
+              beatae ea recusandae blanditiis veritatis.
+            </p>
+          </div>
+
+          {/* Footer Links */}
+          <div className="col-span-2 grid grid-cols-2 sm:grid-cols-3 md:pl-10">
+            <div>
+              <div className="px-4 py-8">
+                <h1 className="mb-3 text-justify text-xl font-bold sm:text-left sm:text-xl">
+                  Important Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li
+                      className="cursor-pointer text-gray-200 duration-300 hover:translate-x-1 hover:text-primary"
+                      key={link.title}
+                    >
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div>
+              <div className="px-4 py-8">
+                <h1 className="mb-3 text-justify text-xl font-bold sm:text-left sm:text-xl">
+                  Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li
+                      className="cursor-pointer text-gray-200 duration-300 hover:translate-x-1 hover:text-primary"
+                      key={link.title}
+                    >
+                      <span>{link.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            {/* social links */}
+
+            <div>
+              <div className="mt-6 flex items-center gap-3">
+                <a href="#">
+                  <FaInstagram className="text-3xl" />
+                </a>
+                <a href="#">
+                  <FaFacebook className="text-3xl" />
+                </a>
+                <a href="#">
+                  <FaLinkedin className="text-3xl" />
+                </a>
+              </div>
+              <div className="mt-6">
+                <div className="flex items-center gap-3">
+                  <FaLocationArrow />
+                  <p>Noida, Uttar Pradesh</p>
+                </div>
+                <div className="mt-3 flex items-center gap-3">
+                  <FaMobileAlt />
+                  <p>+91 123456789</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DefaultFooter;
