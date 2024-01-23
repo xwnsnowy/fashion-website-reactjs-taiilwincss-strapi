fddd6687dcc2e191dc504b59aa1f18dfabffd680
Nguyen Tien Thanh <thanhnthe163528@fpt.edu.vn>
2024-01-23 11:08:39 +0700

    feat: implement Recommend component in product detail page

--- src/pages/ProductDetail/Recommended/Recommended.jsx
+++ src/pages/ProductDetail/Recommended/Recommended.jsx
@@ -1,12 +1,34 @@
+import images from "../../../assets/images";
+
 const Recommended = () => {
   return (
-    <div>
-      <h2>recommended for you</h2>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className="container mb-14 mt-24">
+      <h2 className="mb-6 text-xl font-bold uppercase">recommended for you</h2>
+      <div className="grid gap-10 sm:grid-cols-2 xl:grid-cols-4">
+        <div>
+          <img src={images.shirt1} alt="" />
+          <h3 className="text-base font-bold uppercase">Balenciaga</h3>
+          <p className="font-normal">Turtleneck long dress</p>
+          <p className="mt-8 font-bold">$3,200</p>
+        </div>
+        <div>
+          <img src={images.shirt1} alt="" />
+          <h3 className="text-base font-bold uppercase">Balenciaga</h3>
+          <p className="font-normal">Turtleneck long dress</p>
+          <p className="mt-8 font-bold">$3,200</p>
+        </div>
+        <div>
+          <img src={images.shirt1} alt="" />
+          <h3 className="text-base font-bold uppercase">Balenciaga</h3>
+          <p className="font-normal">Turtleneck long dress</p>
+          <p className="mt-8 font-bold">$3,200</p>
+        </div>
+        <div>
+          <img src={images.shirt1} alt="" />
+          <h3 className="text-base font-bold uppercase">Balenciaga</h3>
+          <p className="font-normal">Turtleneck long dress</p>
+          <p className="mt-8 font-bold">$3,200</p>
+        </div>
       </div>
     </div>
   );
